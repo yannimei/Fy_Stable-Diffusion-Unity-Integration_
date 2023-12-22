fa6ab57ebe33f2e97090f67cac06bb6aafdd0150
yannimei <mei_yanni@163.com>
2023-12-22 11:56:47 +0100

    transcript speed and create prompt
    now woking on ui canvas

--- Assets/Script/ChatGptForPrompt.cs
+++ Assets/Script/ChatGptForPrompt.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using OpenAI;
 
 public class ChatGptForPrompt : MonoBehaviour
@@ -8,11 +9,15 @@ public class ChatGptForPrompt : MonoBehaviour
     private OpenAIApi openAI = new OpenAIApi();
     private List<ChatMessage> messages = new List<ChatMessage>();
 
+    public Text output;
+
     // Start is called before the first frame update
-    public async void AskChatGPT(string newText)
+    public async void AskChatGPT(Text newText)
     {
         ChatMessage newMessage = new ChatMessage();
-        newMessage.Content = newText;
+        newMessage.Content = newText.text;
+        
+
         newMessage.Role = "user";
 
         messages.Add(newMessage);
@@ -28,7 +33,8 @@ public class ChatGptForPrompt : MonoBehaviour
             var chatResponse = response.Choices[0].Message;
             messages.Add(chatResponse);
 
-            Debug.Log(chatResponse.Content);
+            //Debug.Log(chatResponse.Content);
+            output.text = chatResponse.Content;
         }
     }
 
